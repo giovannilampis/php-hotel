7617696bebdfaa68d9cc745fcabef0d55ba7964f
giovannilampis <giolampis@hotmail.com>
2023-05-19 15:46:03 +0200

    by a foreach cycle, print in html every hotel name

--- index.php
+++ index.php
@@ -41,3 +41,23 @@ $hotels = [
 ];
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-hotel</title>
+</head>
+
+<body>
+
+    <?php foreach ($hotels as $hotel) {
+        echo "Nome: " . $hotel['name'] . "<br>";
+    } ?>
+
+</body>
+
+</html>
